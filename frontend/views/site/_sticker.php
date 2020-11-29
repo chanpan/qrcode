8eadbb59b214ee0871ecd52b7f6ec8d39d00c1ea
nuttaphon <chanpan.nuttaphon@gmail.com>
2020-11-29 19:08:06 +0700

    qrcode link

--- frontend/views/site/_sticker.php
+++ frontend/views/site/_sticker.php
@@ -1,5 +1,5 @@
 <?php
-$qr = 'http://project4.local/cars/view?id='.$id;
+$qr = 'http://qrcode.newriched.com/cars/view?id='.$id;
 //$name = isset($car['T_name'])?$car['T_name']:'';
 //$home = isset($car['T_home'])?$car['T_home']:'';
 //$tombon = isset($car['T_district'])?$car['T_district']:'';
